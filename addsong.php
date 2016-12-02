652a64140c9e546e14f8b52a330e82e0f4ba1518
Akshatsrivastava2 <akshatsrivastava2@gmail.com>
2016-12-02 16:45:50 +0530

    login registration and download button

---
+++ addsong.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+if(!isset($_SESSION['user']))
+{
+	header("location:login.php");
+}
+else
+{
+?>
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+	$userid=$_SESSION['user'];
+	$songname=$_POST['download'];
+	$songid=0;
+	$conn=new mysqli("localhost","root","","musicplayer");
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+	} 
+	$sql1="SELECT songid FROM songs WHERE title='$songname'";
+	$result = $conn->query($sql1);
+
+	if ($result->num_rows ==1)
+	{
+		$row=$result->fetch_assoc();
+		$songid=$row["songid"];
+	}
+	$sql= "INSERT INTO usersong(userid,songid) VALUES('$userid','$songid')";
+	echo $conn->query($sql);
+
+	?>
+	Download :
+	<input type="button" name="download" value="Download" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+</body>
+</html>
+<?php } ?>
